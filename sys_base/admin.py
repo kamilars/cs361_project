d592ce475862b6956441bb31922d51ac99a254d3
Kamila <arslanova.25.09.00@gmail.com>
2022-11-30 22:19:48 +0600

    request form updated with time slots

--- sys_base/admin.py
+++ sys_base/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Patient, Doctor, AdminStaff, AppointmentRequest
+from .models import Patient, Doctor, AdminStaff, AppointmentRequest,Appointment
 
 # Register your models here.
 
 admin.site.register(Patient)
 admin.site.register(Doctor)
 admin.site.register(AdminStaff)
-admin.site.register(AppointmentRequest)
+#admin.site.register(AppointmentRequest)
+admin.site.register(Appointment)
